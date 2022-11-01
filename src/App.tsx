bf5809bbace70382c7e9736d275b95d557ff4523
KelvinQiu <kelvinqiu802@outlook.com>
2022-11-01 20:21:12 +0800

    feat: input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  const [input, setInput] = React.useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setInput(e.currentTarget.value);
+  };
+
+  return (
+    <div className='App'>
+      <h1>Todo List with React and TypeScript</h1>
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={input} onChange={handleChange} />
+        <button type='submit'>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
